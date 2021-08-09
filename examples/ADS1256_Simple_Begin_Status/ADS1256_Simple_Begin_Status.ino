b6b18d545586acf99895f9634958e08ca9a5ec73
Axel <ansepulveda@uc.cl>
2021-08-09 13:11:44 -0400

    status outputs 110000

--- examples/ADS1256_Simple_Begin_Status/ADS1256_Simple_Begin_Status.ino
+++ examples/ADS1256_Simple_Begin_Status/ADS1256_Simple_Begin_Status.ino
@@ -2,6 +2,7 @@
 // Written by Axel Sepulveda, May 2020
 
 #include <ADS1256.h>
+//#include "ADS1256.h" // Use relative path 
 #include <SPI.h>
 
 float clockMHZ = 7.68; // crystal frequency used on ADS1256
@@ -22,9 +23,7 @@ void setup()
   adc.begin(); 
   ADCstatus= adc.getStatus();
   Serial.print("ADC Started: ");
-  delay(1000);
-  
-  Serial.println(ADCstatus,BIN);   // print in binary format
+  Serial.println(ADCstatus,BIN);   // print in binary format, ADS1256 should print "110000"
 }
 
 void loop()
