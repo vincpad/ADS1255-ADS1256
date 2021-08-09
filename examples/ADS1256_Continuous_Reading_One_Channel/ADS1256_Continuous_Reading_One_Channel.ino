c0e384ec9d82c13edefb1d1c5cc56ef7617c659f
Axel <ansepulveda@uc.cl>
2021-08-09 15:15:53 -0400

    fix comments and clean

--- examples/ADS1256_Continuous_Reading_One_Channel/ADS1256_Continuous_Reading_One_Channel.ino
+++ examples/ADS1256_Continuous_Reading_One_Channel/ADS1256_Continuous_Reading_One_Channel.ino
@@ -1,7 +1,7 @@
 // Arudino Sample Code to use ADS1256 library
 // http://www.ti.com/lit/ds/symlink/ads1256.pdf
 
-// Reads 1 channel continuously
+// Reads channel 0 continuously
 
 // Written by Axel Sepulveda, May 2020
 
@@ -16,14 +16,15 @@ ADS1256 adc(clockMHZ,vRef,false); // RESETPIN is permanently tied to 3.3v
 
 float sensor1;
 
-
 void setup()
 {
   Serial.begin(250000);
   
   Serial.println("Starting ADC");
 
-  // start the ADS1256 with data rate of 15 SPS
+  // start the ADS1256 with data rate of 15 SPS and gain x1
+  adc.begin(ADS1256_DRATE_15SPS,ADS1256_GAIN_1,false); 
+    
   // other data rates: 
   // ADS1256_DRATE_30000SPS
   // ADS1256_DRATE_15000SPS
@@ -52,18 +53,18 @@ void setup()
   //ADS1256_GAIN_16 
   //ADS1256_GAIN_32 
   //ADS1256_GAIN_64 
-  adc.begin(ADS1256_DRATE_15SPS,ADS1256_GAIN_1,false); 
-
+  
   Serial.println("ADC Started");
   
-   // Set MUX Register to AINO so it start doing the ADC conversion
-  adc.setChannel(0,1);
+  // Set MUX Register to AINO so it start doing the ADC conversion
+  Serial.println("Channel set to Single end ch0");
+  adc.setChannel(0);
 }
 
 void loop()
 { 
-  adc.waitDRDY(); // wait for DRDY to go low before changing multiplexer register 
-  sensor1 = adc.readCurrentChannel(); // DOUT arriving here is from MUX AIN0 and AIN8 (GND)
-  //print the result.
-  Serial.println(sensor1,10);
+  adc.waitDRDY(); // wait for DRDY to go low before next register read
+  sensor1 = adc.readCurrentChannel(); // read as voltage according to gain and vref
+  
+  Serial.println(sensor1 , 10); // Print as decimal, 10 decimal places
 }
